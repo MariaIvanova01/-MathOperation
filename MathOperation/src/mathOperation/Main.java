da5658614e6dd1474e990e476e691967545b07fa
MariaIvanova01 <mimi200801@abv.bg>
2022-08-01 13:26:23 +0300

    Method overloading implemented

--- MathOperation/src/mathOperation/Main.java
+++ MathOperation/src/mathOperation/Main.java
@@ -3,6 +3,10 @@ package mathOperation;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        MathOperation mathOperation = new MathOperation();
+
+        System.out.println(mathOperation.add(2,2));
+        System.out.println(mathOperation.add(3,3,3));
+        System.out.println(mathOperation.add(4,4,4,4));
     }
 }
